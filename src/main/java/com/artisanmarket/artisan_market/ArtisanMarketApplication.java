1c1f3a70220ced89d6aac684f6f19b63bc401085
Shukrah-036 <adekumbi036@gmail.com>
2026-03-03 16:41:01 +0000

    Initial Commit

---
+++ src/main/java/com/artisanmarket/artisan_market/ArtisanMarketApplication.java
@@ -0,0 +1,13 @@
+package com.artisanmarket.artisan_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisanMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artisanmarket/artisan_market/ArtisanMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artisanmarket.artisan_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
